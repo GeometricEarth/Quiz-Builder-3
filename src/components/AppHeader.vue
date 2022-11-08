b237b1476a63012de5ee3b9e3397ab395852d8ab
GeometricEath <geo.fsv@gmail.com>
2022-11-08 12:33:58 +0300

    Модальное предупрежедение о несохраненных данных при переходе домой

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,31 @@
 <template>
-  <v-toolbar class="primary">
-    <v-toolbar-title>Test</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items>
-      <!-- <v-btn text fab to="/"> -->
-      <v-btn text fab @click="goHome">
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-      <v-btn text fab to='/save-project'>
-        <v-icon>mdi-content-save</v-icon>
-      </v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+  <v-main>
+
+    <v-toolbar class="primary">
+      <v-toolbar-title>QuizBuilder 3</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <!-- <v-btn text fab to="/"> -->
+          <v-btn :disabled="disableHomeBtn" text fab @click="goHome">
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+          <v-btn text fab to='/save-project'>
+            <v-icon>mdi-content-save</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+        </v-toolbar>
+        <v-dialog v-model="dialog" max-width="500">
+          <v-card>
+            <v-card-title class=" grey lighten-2">Похоже есть не схорнаненные изменения</v-card-title>
+            <v-card-text>Удалить несохраненные изменения перед переходом?</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click.native="dialog = false">Отменить</v-btn>
+              <v-btn text color="primary" @click.native="reset">Удалить</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+    </v-main>
 </template>
 
 <script>
@@ -19,15 +33,32 @@
 
 export default {
   data() {
-    return {};
+    return {
+      disableHomeBtn: false,
+      dialog: false
+    };
   },
+  // beforeMount(){
+  //   if (this.$route.path == '/') {
+  //     this.disableHomeBtn = true;
+  //     console.log('true')
+  //   }
+  // },
   methods: {
     save() {
       this.$bus.emit("saveProject");
     },
     goHome() {
-      this.$store.commit("RESET_STORE");
-      this.$router.push({name:'home'})
+      if(this.$store.getters.questions.length > 0){
+        this.dialog = true;
+        return
+      }
+      this.$router.push('/')
+    },
+    reset(){
+      this.dialog = false;
+      this.$store.commit('RESET_STORE');
+      this.$router.push('/')
     }
   }
 };
